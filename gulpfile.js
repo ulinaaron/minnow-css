f74f9f0b33177b6e552d1bf7c0f67b2881b9675e
Aaron <ulinaaron@gmail.com>
2014-03-18 22:58:51 -0400

    A little cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    sass = require('gulp-ruby-sass');
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css'),
+    rename = require('gulp-rename');
 
-gulp.task('sass', function() {
+// Compiles Sass
+gulp.task('styles', function() {
     gulp.src('./sass/*.scss')
         .pipe(sass())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest('./css'));
 });
 
-gulp.task('default', ['sass'], function() {
+// Minify CSS
+gulp.task('css', function() {
+    gulp.src('./css/*.css')
+        .pipe(minifyCSS(opts))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./css'));
+});
+
+// Default Tasks
+
+gulp.task('default', ['styles', 'css'], function() {
     // Default task code
 });
