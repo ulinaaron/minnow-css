053f6fe1e33d6982d07c8f26b509a0c0b28326f3
Aaron Mazade <aaron@aaronmazade.com>
2014-08-23 18:29:32 -0400

    Updating task processes

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,213 @@
+/**
+ * Setup
+ * ========================
+ */
+
+    // Source
+var dir_scss                    = 'src/scss/',
+    dir_scss_vendor             = 'src/scss/vendor/',
+    dir_css                     = 'src/css/',
+    // Docs Setup
+    dir_docs                    = 'docs/',
+    dir_docs_src                = 'docs/src/',
+    dir_docs_build              = 'docs/build/',
+    // Docs Source
+    dir_docs_src_assets         = 'docs/src/assets/',
+    dir_docs_src_scss           = 'docs/src/assets/scss/',
+    dir_docs_src_js             = 'docs/src/assets/js/',
+    dir_docs_src_js_standalone  = 'docs/src/assets/js/standalone/',
+    dir_docs_src_js_plug        = 'docs/src/assets/js/plugins/',
+    dir_docs_src_img            = 'docs/src/assets/img/',
+    // Docs Build
+    dir_docs_build_css          = 'docs/build/assets/css/',
+    dir_docs_build_js           = 'docs/build/assets/js/',
+    dir_docs_build_img          = 'docs/build/assets/img/',
+    // Misc
+    dir_bower                   = 'src/bower_components/',
+    dir_npm                     = 'node_modules/',
+    js_final                    = 'main', // JS final name of all the combined JS files
+    // Browser Sync Settings
+    dev_port                    = '7280',
+    dev_dir                     = dir_docs_build
+;
+
+/**
+ * Initialize
+ * ========================
+ */
+
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifyCSS = require('gulp-minify-css'),
-    rename = require('gulp-rename');
+    plugins = require('gulp-load-plugins')({
+        camelize: true
+    }),
+    deploy = require('gulp-gh-pages'),
+    merge = require('merge-stream'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    build = '';
 
-// Compiles Sass
-gulp.task('styles', function() {
-    gulp.src('./sass/*.scss')
-        .pipe(sass())
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest('./css'));
+/**
+ * Task: Source Styles
+ * ========================
+ * Compiles Sass (scss) for source
+ */
+
+gulp.task('src-styles', function () {
+    return gulp.src([dir_scss + '*.scss', '!' + dir_scss + '_*.scss'])
+    .pipe(plugins.sass({
+        errLogToConsole: true
+    }))
+    .pipe(plugins.autoprefixer('last 2 versions', 'ie 9', 'ios 6', 'android 4'))
+    .pipe(plugins.bless())
+    .pipe(gulp.dest(dir_css))
+    .pipe(reload({stream:true}))
+    .pipe(plugins.minifyCss({
+        keepSpecialComments: 1
+    }))
+    .pipe(plugins.rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest(dir_css))
+    .pipe(reload({stream:true}));
 });
 
-// Minify CSS
-gulp.task('css', function() {
-    gulp.src('./css/*.css')
-        .pipe(minifyCSS(opts))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('./css'));
+/**
+ * Task: Doc Styles
+ * ========================
+ */
+
+gulp.task('doc-styles', function () {
+    return gulp.src([dir_docs_src_scss + '*.scss', '!' + dir_docs_src_scss + '_*.scss'])
+    .pipe(plugins.sass({
+        errLogToConsole: true
+    }))
+    .pipe(plugins.autoprefixer('last 2 versions', 'ie 9', 'ios 6', 'android 4'))
+    .pipe(plugins.bless())
+    .pipe(gulp.dest(dir_docs_build_css))
+    .pipe(reload({stream:true}))
+    .pipe(plugins.minifyCss({
+        keepSpecialComments: 1
+    }))
+    .pipe(plugins.rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest(dir_docs_build_css))
+    .pipe(reload({stream:true}));
 });
 
-// Default Tasks
+/**
+ * Task: Style Testing
+ * ========================
+ */
 
-gulp.task('default', ['styles', 'css'], function() {
-    // Default task code
+gulp.task('style-test', function() {
 });
+
+/**
+ * Task: Clean
+ * ========================
+ */
+
+gulp.task('clean', function () {
+    return gulp.src(build + '**/.DS_Store', {
+        read: false
+    })
+    .pipe(plugins.rimraf());
+});
+
+/**
+ * Task: Reload
+ * ========================
+ */
+
+gulp.task('reload', function () {
+    browserSync.reload();
+});
+
+
+/**
+ * Task: Bower Packages
+ * ================
+ * This is a manual process for components that should be included.
+ * However the advantage is Gulp will fetch the latest version on bower update.
+ * This function is not included in the default Gulp process.
+ * Run 'gulp bower' to use.
+ */
+
+gulp.task('bower-packages', function () {
+
+    return merge(
+
+        // Normalize
+        gulp.src([dir_bower + 'normalize.css/normalize.css'])
+        .pipe(plugins.rename('_base_normalize.scss'))
+        .pipe(gulp.dest(dir_scss_vendor)) // Copies to src/scss
+
+    );
+
+});
+
+/**
+ * Task: NPM Components
+ * ================
+ * This is a manual process for components that should be included.
+ * This function is not included in the default Gulp process.
+ * Run 'gulp npm-packages' to use.
+ */
+
+gulp.task('npm-packages', function () {
+
+    return merge();
+});
+
+/**
+ * Task: Deploy
+ * ========================
+ * Deployes to Github Pages
+ */
+
+gulp.task('deploy', function() {
+    gulp.src(dir_docs_build + '**/*')
+    .pipe(deploy());
+});
+
+/**
+ * Task: Jade
+ * ========================
+ * Compiles Jade to HTML
+ */
+
+gulp.task('jade', function() {
+    gulp.src(dir_docs_src + '*.jade')
+    .pipe(plugins.jade())
+    .pipe(gulp.dest(dir_docs_build))
+    .pipe(reload({stream:true}));
+});
+
+/**
+ * Task: Watch
+ * ========================
+ * Initiates BrowserSync and watches files for any tasks.
+ */
+
+gulp.task('watch', function() {
+
+    browserSync.init({
+        server: {
+            baseDir: dev_dir
+        },
+        open: false
+    });
+
+    gulp.watch(dir_scss + '**/*.scss', ['src-styles', 'doc-styles']);
+    gulp.watch(dir_docs_src_scss + '**/*.scss', ['doc-styles']);
+    gulp.watch(dir_docs_src + '**/*.jade', ['jade']);
+});
+
+/**
+ * Task: Default
+ * ========================
+ */
+
+gulp.task('default', ['jade', 'src-styles', 'clean', 'watch']);
